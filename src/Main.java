7036c13d26323af4cd170bd6a0fa2d14db6507e6
ilyas <zhuanyshev.ilyas@gmail.com>
2023-12-09 07:50:32 +0600

    My Commit

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,12 @@ public class Main {
         user.setFullName("Ilyas Zhuanyshev");
         user.setBirthdate(LocalDate.of(1989, 5,26));
 
-        System.out.println(user);
+        User user2 = new User();
+        user2.setEmail("shaima@gmail.com");
+        user2.setFullName("Shaima Ibrahim");
+        user2.setBirthdate(LocalDate.of(1995, 12,31));
+
+        System.out.println(user2);
 
     }
 }
